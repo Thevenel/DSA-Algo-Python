605376921e03cdce54ee0906e2f509df4d34eeb7
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-10-17 14:56:34 +0200

    implemented first step in merge sort algo

--- merge_sort.py
+++ merge_sort.py
@@ -3,6 +3,12 @@ def merge_sort(arr1, arr2):
     print(f"left: {arr1} and right {arr2}")
     sorted_array = []
     i, j = 0, 0
+    if arr1[i] < arr2[j]:
+        sorted_array.append(arr1[i])
+        i += 1
+    else:
+        sorted_array.append(arr2[j])
+        j += 1
     print(f"Left list index i is {i} and has value {arr1[i]}")
     print(f"Right list index j is {j} and has value {arr2[j]}")
     return sorted_array
@@ -14,5 +20,15 @@ l1 = [1, 4, 6, 8, 10]
 l2 = [2, 3, 5, 7, 8, 9]
 
 print(f"Un-merged-list: {merge_sort(l1, l2)}")
+
+
+# Steps
+# 1. Compare first element in each list and append smaller element
+# 2. Using two indices and an iterator to perform same comparison for 
+#  for all elements in both lists
+# 3. Move marker up by 1 position when the smaller number is found
+# 4. Copy remaining lists once comparisons are completed and there 
+# are items still remaining in one a the lists
+
 
 
