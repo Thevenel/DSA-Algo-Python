90b3f9f17fabe59f709e41ae0c9694d1826d56a5
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-11-01 14:53:21 +0100

    added dynamic run time

--- analyzer/performance_analyzer.py
+++ analyzer/performance_analyzer.py
@@ -16,17 +16,21 @@ def analyze_func(func_name, arr):
     seconds = end_time - start_time
     print(f"{func_name.__name__.capitalize()}\t-> Runtime : {seconds}")
 
-size = int(input("What is the size of the list you want to create "))
-max_val = int(input("What is the max value of the range "))
+size = int(input("What is the size of the list you want to create? "))
+max_val = int(input("What is the max value of the range? "))
+run_times = int(input("How many times you want it to run? "))
+
+for num in range(run_times):
+    print(f"Run: {num+1}")
+    l = generate_random_list(size, max_val)
+    # Because bubble sort makes in-place sort, we'll call it
+    # with a copy to not modify our original list
+    analyze_func(bubblesort, l.copy())
+    analyze_func(quicksort, l)
+    analyze_func(mergesort, l)
+    print("-" * 40)
 
 # print(generate_random_list(size, max_val))
 
-l = generate_random_list(size, max_val)
-
-# Because bubble sort makes in-place sort, we'll call it
-# with a copy to not modify our original list
-analyze_func(bubblesort, l.copy())
-analyze_func(quicksort, l)
-analyze_func(mergesort, l)
 
 
