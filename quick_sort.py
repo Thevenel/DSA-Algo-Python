e3dfd919df1de6e7e30475498c62baae2d275cf7
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-11-01 12:31:58 +0100

    finished quick sort implementation (not in-place);

--- quick_sort.py
+++ quick_sort.py
@@ -4,7 +4,21 @@ def quick_sort(arr):
     Returns sorted list of integers using Quicksort
     Note: This is not an in-place implementation.
     """
-    return arr
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[-1]
+        smaller, equal, larger = [], [], []
+        for num in arr:
+            if num < pivot:
+                smaller.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                larger.append(num)
+        # We can call quicsort on the smaller and larger list
+        # and add it to the equal list and we'll get the sorted list
+        return quick_sort(smaller) + equal + quick_sort(larger) #return a list by change , to +
 
 l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
 print(quick_sort(l))
