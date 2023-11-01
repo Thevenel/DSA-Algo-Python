041c4761955b37f5b38d831a26701f54c741334c
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-11-01 12:02:43 +0100

    setup quicksort implementation

---
+++ quick_sort.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    """
+    Input: Unsorted list of integers 
+    Returns sorted list of integers using Quicksort
+    Note: This is not an in-place implementation.
+    """
+    return arr
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+print(quick_sort(l))
