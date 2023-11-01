021f8c61a8bb6800a77e0ee34dcbcb02c7ad0911
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-11-01 14:16:06 +0100

    calculated the runtime for algo

--- analyzer/demos.py
+++ analyzer/demos.py
@@ -1,1 +1,50 @@
 print("Algorithms file load")
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[-1]
+        smaller, equal, larger = [], [], []
+        for num in arr:
+            if num < pivot:
+                smaller.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                larger.append(num)
+        return quick_sort(smaller) + equal + quick_sort(larger)
+    
+# Merge Sort
+def merge_sort(arr1, arr2):
+# print("Merge function with lists bellow:")
+# print(f"left: {arr1} and right {arr2}")
+    sorted_array = []
+    i, j = 0, 0
+    while i < len(arr1) and j < len(arr2):
+        # print(f"Left list index i is {i} and has value {arr1[i]}")
+        # print(f"Right list index j is {j} and has value {arr2[j]}")
+        if arr1[i] < arr2[j]:
+            sorted_array.append(arr1[i])
+            i += 1
+        else:
+            sorted_array.append(arr2[j])
+            j += 1
+        # print(sorted_array)
+    while i < len(arr1):
+        sorted_array.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        sorted_array.append(arr2[j])
+        j += 1
+    return sorted_array
+# the function to call
+def mergesort(arr):
+    if len(arr) < 2:
+        # print(f"Base case condition reached with: {arr[:]}")
+        return arr[:]
+    else:
+        middle = len(arr)// 2 # divide the array into 2 part
+        l1 = mergesort(arr[:middle]) # take the left part 
+        l2 = mergesort(arr[middle:]) # take the right part
+        return merge_sort(l1, l2) # take l1 and l2 merge them into single sorted list
--- analyzer/performance_analyzer.py
+++ analyzer/performance_analyzer.py
@@ -1,4 +1,5 @@
-import demos
+import time
+from demos import quick_sort, mergesort
 from random import randint
 
 def generate_random_list(size, max_val):
@@ -11,4 +12,16 @@ def generate_random_list(size, max_val):
 
 size = int(input("What is the size of the list you want to create "))
 max_val = int(input("What is the max value of the range "))
-print(generate_random_list(size, max_val))
+
+# print(generate_random_list(size, max_val))
+
+l = generate_random_list(size, max_val)
+start_time = time.time()
+quick_sort(l)
+end_time = time.time()
+print(f"QS Elapse time : {end_time - start_time}")
+
+start_time = time.time()
+mergesort(l)
+end_time = time.time()
+print(f"MS Elapse time : {end_time - start_time}")
