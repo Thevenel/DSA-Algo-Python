e2df7643a003af38088f9da3b01f00e9d39e0a49
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-11-15 07:28:17 +0100

    finished recursive implementation for bs

---
+++ binarysearch_rec.py
@@ -0,0 +1,22 @@
+def binary_search_rec(n, arr, start, stop):
+    if start > stop:
+        return f"{n} not found in the list."
+    else:
+        mid = (start + stop)//2
+        if n == arr[mid]:
+            return f"{n} found at index {mid}"
+        elif n > arr[mid]:
+            return binary_search_rec(n, arr, mid+1, stop)
+        else:
+            return binary_search_rec(n, arr, start, mid-1)
+
+def create_list(max_val):
+    arr = []
+    for num in range(max_val):
+        arr.append(num)
+    return arr
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#ind 0  1  2  3  4  5  6  7  8  9
+num_to_search = 8
+print(binary_search_rec(num_to_search, l, 0, len(l)-1))
